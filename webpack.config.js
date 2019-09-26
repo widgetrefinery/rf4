8bbd4001274d881ed4f882b8660573ea6a5943f6
widgetrefinery <widgetrefinery@gmail.com>
2019-09-26 02:13:25 -0700

    better css packaging

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,56 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
+
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/index.js'
-    },
-    devtool: 'eval-source-map',
+    mode: devMode ? 'development' : 'production',
+    entry: './src/index.js',
+    devtool: devMode ? 'eval-source-map' : false,
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[contenthash].css',
+            chunkFilename: '[contenthash].css'
         })
     ],
     output: {
         filename: '[contenthash].js',
+        chunkFilename: '[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: { hmr: devMode }
+                }, 'css-loader']
             }
         ]
     },
     optimization: {
         moduleIds: 'hashed',
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin()
+        ],
         splitChunks: {
             chunks: 'all',
             cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all'
+                },
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
                     name: 'vendors',
